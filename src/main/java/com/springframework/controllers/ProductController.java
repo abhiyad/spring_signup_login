8f249c84c9178547c3a50f6d99746f00d5ddda06
abhiyad <abhishekcms611@gmail.com>
2020-04-21 20:18:50 +0530

    working storing and retrieval

--- src/main/java/com/springframework/controllers/ProductController.java
+++ src/main/java/com/springframework/controllers/ProductController.java
@@ -1,29 +1,49 @@
 package com.springframework.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.springframework.domain.User;
+import com.springframework.domain.Person;
+import com.springframework.repositories.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jt on 1/10/17.
  */
 @Controller
 public class ProductController {
+    @Autowired
+    UserRepository repository;
 
     @GetMapping("/addUser")
-    public String sendForm(User user){
+    public String sendForm(Person person){
         return "addUser";
     }
     @PostMapping("/addUser")
-    public String process_form(User user){
-        String fname = user.getFname();
-        String lname = user.getLname();
+    public String process_form(Person person){
+        String fname = person.getFname();
+        String lname = person.getLname();
+        Person u = new Person(fname,lname);
+        repository.save(u);
         System.out.println("++++++++++ " + fname + " " + lname);
+        System.out.println("CURRENT DATABASE ::: ");
+        List<Person> personList = repository.findAll();
+        for (Person p : personList) {
+            System.out.println("PERSON : " + p.getFname() + " " + p.getLname());
+        }
         return "showUser";
     }
+    @GetMapping("showAll")
+    public String showAll(Model model){
+        model.addAttribute("person",repository.findAll());
+        return "showAll";
+    }
     @RequestMapping("/")
-    public String index(User User){
-        return "addUser";
+    public String index(Person person) {
+        return "index";
     }
 }
